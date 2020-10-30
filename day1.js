6e50885f53ab5c93a8c008fdcb43b5c1eeae1f84
obinnaeye <obinnaeye@gmail.com>
2020-10-30 15:49:45 +0100

    Initial commit

---
+++ day1.js
@@ -0,0 +1,35 @@
+// var name = 'Chinecherem';
+
+// var date = new Date()
+
+// console.log("Today's date and time is " + date)
+// console.log('My name is ' + name);
+
+
+
+// 1. Array (List)
+var items1 = ['bag', 'meat', 5, ['sugar', 'garri', 'peanuts']]
+
+var items2 = ['shoes', 'dress', 'milk', 'shoes']
+
+console.log('...........')
+console.log(items2)
+console.log('...........')
+
+// 2. Set: A list of unique items
+
+// 3. Object (JSON) == JavaScript Object Notation; Objects are also called Dictionaries 
+
+var address = {
+    street: '7 eziana street',
+    city: 'Awka',
+    lga: 'Awka south',
+    state: 'Anambra',
+    country: 'Nigeria',
+    postalCode: 100445
+}
+
+
+console.log(address.postalCode)
+
+// Iterations and Recursion
